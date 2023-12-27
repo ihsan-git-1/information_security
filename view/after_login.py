47f4d2089997c95b47f2488244eb91e00cb5bd7f
ihsan-git-1 <ihsan.abourshaid.git1@gmail.com>
2023-12-27 20:41:32 +0400

    added encryption for edit

--- view/after_login.py
+++ view/after_login.py
@@ -1,5 +1,7 @@
 
 from app_sockets.client_module import client_send_json_message
+from encryptions.aes_encryption import AesEncryption
+from utils import convert_string_to_key
 
 
 def options_after_login_view(username):
@@ -20,12 +22,19 @@ def edit_view(username):
     new_city = input("Enter new city: ")
     new_phone_number = input("Enter new phone number: ")
 
+    aes = AesEncryption(convert_string_to_key(username))
+    city_encrypted = aes.encrypt(new_city)
+    phone_encrypted = aes.encrypt(new_phone_number)
+
     edit_request = {
         "route": "edit",
         "parameters": {
             "username": username,
-            "city": new_city,
-            "phone_number": new_phone_number
+            "city": city_encrypted.decode(),
+            "phone_number": phone_encrypted.decode()
         }
     }
+    print("city"+ str(city_encrypted))
+    print("phone_number"+ str(phone_encrypted))
+
     serverResponse = client_send_json_message(edit_request)
