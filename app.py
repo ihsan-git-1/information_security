e7bd17837f55f34a209ef5508523eb7655a867b0
ihsan-git-1 <ihsan.abourshaid.git1@gmail.com>
2023-12-27 14:25:45 +0400

    clean code

--- app.py
+++ app.py
@@ -1,24 +1,8 @@
 
-import os
 import socket
-import threading
-from flask import Flask as FlaskAuth
-from pathlib import Path
-from sqlalchemy.ext.declarative import declarative_base
-from dotenv import load_dotenv
-from sqlalchemy import MetaData, create_engine
 
-from accounts.methods.choose_user_type import choose_client_type
+from methods.choose_user_type import choose_client_type
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "accounts", "static", "assets")
-
-UPLOAD_FOLDER = os.path.join(MEDIA_ROOT, "profile")
-
-metadata = MetaData()
 
 host = "127.0.0.1"
 port = 12345
@@ -26,7 +10,7 @@ port = 12345
 choice = input("Do you want server(1) or client(2): ")
 
 if choice == "1":
-    # Create a socket server
+    # Create the socket of the server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
 
@@ -40,6 +24,7 @@ elif choice == "2":
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientSocket = clientSocket.connect((host, port))
 
+    # choose client type 
     choose_client_type(clientSocket)
 
 else:
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import base64
 import hashlib
-from werkzeug.utils import secure_filename
 import uuid
 import secrets
 import os
@@ -10,18 +9,6 @@ def unique_uid():
 
 def unique_security_token():
     return str(secrets.token_hex())
-
-def get_unique_filename(filename=None):
-    if not filename:
-        return None
-        
-    filename = secure_filename(filename).split(".")
-    return "{}.{}".format(str(uuid.uuid4()), filename[len(filename)-1])
-
-def remove_existing_file(path=None):
-    if os.path.isfile(path=path):
-        os.remove(path)
-
 
 def convert_string_to_key(input_string):
   # Create a SHA-256 hash object
