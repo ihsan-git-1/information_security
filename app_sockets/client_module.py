36ec61b6d62f9d054996a1c10d68feaba33f2c2c
ritarezek200185 <ritarezek200185@gmail.com>
2023-12-30 00:57:07 +0300

    added secure connection for teacher

--- app_sockets/client_module.py
+++ app_sockets/client_module.py
@@ -2,20 +2,34 @@ import json
 import socket
 import struct
 import threading
+import ssl
 
 # Global variable to store the client socket instance
 client_socket = None
 
-def connect_to_server(host , port):
+
+def connect_to_server(host, port, cert_path=None, key_path=None, force_secure=False):
     global client_socket
-    # Create a socket object
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Connect to the server
-    server_address = (host, port)
-    client_socket.connect(server_address)
+    if force_secure and (not cert_path or not key_path):
+        # User doesn't have a certificate and secure connection is forced
+        print("Secure connection is required, but you don't have a valid certificate yet.")
+        print("Please acquire a certificate and try again.")
+        return  # Terminate connection attempt
 
-def client_send_json_message(fields):    
+    if cert_path and key_path:  # Use secure connection if certificate is available
+        context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        context.load_cert_chain(certfile=cert_path, keyfile=key_path)
+        client_socket = context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+
+    else:  # Use non-secure connection for initial CSR generation/verification
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect to the server
+        server_address = (host, port)
+        client_socket.connect(server_address)
+
+
+def client_send_json_message(fields):
     global client_socket
     # Convert the fields to a JSON string
     message = json.dumps(fields)
@@ -31,16 +45,13 @@ def client_send_json_message(fields):
 
 
 def client_receive_response():
-  # Receive and print the response from the server
+    # Receive and print the response from the server
     response = client_socket.recv(1024)
-    print(response)
     print('\n********* Server **********')
     print(response.decode('utf-8'))
     print('********* Server **********\n')
     return response.decode('utf-8')
 
-
-       
 
 def client_close_connection():
     global client_socket
