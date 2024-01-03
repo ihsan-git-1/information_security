19648d7d2e65e88e9009b4b7fc88b15c5d6e4706
ritarezek200185 <ritarezek200185@gmail.com>
2024-01-03 11:34:17 +0300

    add view marks and marks table

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from app_sockets.server_module import start_socket_server
 from database.database import initalizeDataBaseTables
 from view.choose_user_type import choose_client_type
 from cryptography import x509
-
+from view.after_teacher_login import options_after_teacher_login_view
+from view.auth import handshake,send_session_key
 
 host = "127.0.0.1"
 port = 5001
@@ -16,18 +17,27 @@ initalizeDataBaseTables()
 
 choice = input("Do you want server(1) or ca server(2) or client(3) : ")
 
+
 def initialize_client_threads():
-    #threads of the client after start 
+    # threads of the client after start
 
     # trigger this function choose_client_type
     threading.Thread(target=choose_client_type, args=()).start()
 
-    
+
+def initialize_client_threads_ssl(user_name):
+    # threads of the client after start
+    handshake(user_name)
+    send_session_key()
+    # trigger this function choose_client_type
+    threading.Thread(target=options_after_teacher_login_view(user_name), args=()).start()
+
+
 if choice == "1":
-    asyncio.run(start_socket_server(host, port,False))
+    asyncio.run(start_socket_server(host, port, False))
 
 elif choice == "2":
-    asyncio.run(start_socket_server(host, port2,True))
+    asyncio.run(start_socket_server(host, port2, True))
 
 elif choice == "3":
     # Un Comment the input values in the day of the presentation
@@ -46,13 +56,13 @@ elif choice == "3":
         user_pk = f'csr_module/{user_name}_private.key'
 
         if os.path.exists(user_certificate) and os.path.exists(user_pk):
-            connect_to_server(host, port2,user_certificate,user_pk)
+            connect_to_server(host, port2, user_certificate, user_pk)
             print("Successfully Connected using certificate")
-            initialize_client_threads()
- 
+            initialize_client_threads_ssl(user_name)
+
         else:
             print(f"{user_name} don't have a certificate or private key please verify first ")
-    
+
     else:
         print("There is no such port running")
 
@@ -62,7 +72,3 @@ elif choice == "3":
 
 else:
     exit()
-
-
-
-
